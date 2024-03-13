8c8a48e4e6a757ad1aded85a3ff244a15ce63ce6
aryan <arya.choudhary@swstrategies.com>
2024-03-13 19:31:36 +0530

    design home page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,17 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { LeftNavComponent } from './components/left-nav/left-nav.component';
 
 
 @NgModule({
   declarations: [
-    TopNavComponent
+    TopNavComponent,
+    LeftNavComponent
   ],
   exports:[
- TopNavComponent
+ TopNavComponent,
+ LeftNavComponent
   ],
   imports: [
     CommonModule
