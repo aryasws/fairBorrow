fcf34c881f1973bfd5aaea1e6d8c94e81d9f7e7f
aryan <arya.choudhary@swstrategies.com>
2024-03-14 18:55:51 +0530

    added open button in chatbot box

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
-import { LeftNavComponent } from './components/left-nav/left-nav.component';
 
 
 @NgModule({
   declarations: [
-    TopNavComponent,
-    LeftNavComponent
+    TopNavComponent
   ],
   exports:[
  TopNavComponent,
- LeftNavComponent
   ],
   imports: [
     CommonModule
